53c8d4ce4b1cdd16d397e1892cb0f09faf441725
p-duke <peteraaronduke@gmail.com>
2017-12-17 22:17:15 -0500

    Write up application details and instructions
    Update routes to only index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :products do
-        resources :inventories
+      resources :products, only: [:index, :show] do
+        resources :inventories, only: [:index, :show]
       end
     end
   end
